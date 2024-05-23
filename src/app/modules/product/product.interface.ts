ee90ef69d55cf0914c65ed60068f763df2ea74c0
enayetaitech <mdrehman@365aitech.com>
2024-05-23 11:27:59 +0600

    product schema added

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,6 +1,6 @@
 import { Schema, model, connect } from 'mongoose';
 
-type Variants = {
+export type Variants = {
   type: string;
   value: string;
 }
---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model, connect } from 'mongoose';
+import { Product, Product, Variants } from './product.interface';
+
+
+const variantSchema = new Schema<Variants>({
+  type: {type: String, required: true},
+  value: {type: String, required: true},
+})
+
+const productSchema = new Schema<Product>({
+  name: {type: String, required: true},
+  description: {type: String, required: true},
+  price: {type: Number, required: true},
+  category: {type: String, required: true},
+  tags: [{type: String}],
+  variants: [variantSchema],
+  inventory:{
+    quantity:{type: Number, required: true},
+    inStock: {type: Boolean, required: true},
+  }
+})
+
+
+const Product = model<Product>('Product', productSchema)
