3722a4a16fc4aa5aa3f8ed47e081dd3381845afa
enayetaitech <mdrehman@365aitech.com>
2024-06-07 20:10:14 +0600

    route corrected again

--- dist/app/modules/order/orderRoute.js
+++ dist/app/modules/order/orderRoute.js
@@ -4,10 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OrderRoutes = void 0;
-const express_1 = __importDefault(require("express"));
 const order_controller_1 = require("./order.controller");
+const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+const handleEmail = async (req, res, next) => {
+    // Check if the email parameter exists in the query
+    if (req.query.email) {
+        // If email parameter exists, call getEmailOrder controller function
+        await order_controller_1.OrderControllers.getEmailOrder(req, res);
+    }
+    else {
+        // If email parameter doesn't exist, call allOrders controller function
+        await order_controller_1.OrderControllers.allOrders(req, res);
+    }
+};
 router.post("/", order_controller_1.OrderControllers.newOrder);
-router.get("/", order_controller_1.OrderControllers.getEmailOrder);
-router.get("/", order_controller_1.OrderControllers.allOrders);
+router.get("/", handleEmail);
 exports.OrderRoutes = router;
--- src/app/modules/order/orderRoute.ts
+++ src/app/modules/order/orderRoute.ts
@@ -1,11 +1,22 @@
-import express from 'express'
 import { OrderControllers } from './order.controller'
-
+import express, { Request, Response, NextFunction } from 'express';
 const router = express.Router()
 
+const handleEmail = async (req: Request, res: Response, next: NextFunction) => {
+  // Check if the email parameter exists in the query
+  if (req.query.email) {
+      // If email parameter exists, call getEmailOrder controller function
+      await OrderControllers.getEmailOrder(req, res);
+  } else {
+      // If email parameter doesn't exist, call allOrders controller function
+      await OrderControllers.allOrders(req, res);
+  }
+};
+
 router.post("/", OrderControllers.newOrder)
-router.get("/", OrderControllers.getEmailOrder)
-router.get("/", OrderControllers.allOrders)
+router.get("/", handleEmail);
 
 
 export const OrderRoutes = router
+
+
